8c81bac69a9926e672871ecc0587c62709034d60
Amoswachira <amoswachira50@gmail.com>
2019-05-27 20:45:50 +0300

    165305713-Chore(React Router): Setup react router version 4
    -install react router
    
    -implement routing
    
    -add dummy login route
    
    [starts #165305713]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import login from './components/Login';
+import PageNotFound from './components/NotFoundComponent';
+import Navbar from './components/Navbar';
+import home from './components/home';
 
+const history = createBrowserHistory();
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Immortals Ah Works</h1>
+        <Router history={history}>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/Login" component={login} />
+              <Route component={PageNotFound} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
