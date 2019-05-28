4e5780101649c1775c32d1bce581fd23b54ae4cb
Barclay Koin <Bakley@users.noreply.github.com>
2019-05-28 12:54:59 +0300

    #165305711 Setting up test environment (#7)
    - setting up testing enviromnet
     - Write basic test for reducers
    [Delivers #165305711]

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,mjs}', '!src/redux/store.js', '!src/index.js'],
+  coverageDirectory: 'coverage',
+  setupFiles: ['<rootDir>/setupFiles.js'],
+  moduleFileExtensions: ['js', 'json', 'jsx'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  testURL: 'http://localhost',
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  verbose: false,
+};
