4e248346232b6e7456838d9d21209e41ddae42a5
IssaIan <41931013+IssaIan@users.noreply.github.com>
2019-05-29 08:07:04 +0300

    ch-Install and setup sass pre-processing on the application (#6)
    - Install dependencies
    - Add configurations to webpack.config.js
    - Add sample css to ensure configuration works
    - Install react-bootstrap and bootstrap dependencies
    
    [Finishes #165305715]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+/*jshint esversion: 6 */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+
 
 ReactDOM.render(
   <Provider store={store}>
